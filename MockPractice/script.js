f89b4604ac9d575391c297820428c176eb0b4b41
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-11-10 16:42:40 +0530

    feat:added code for 200 status

--- MockPractice/script.js
+++ MockPractice/script.js
@@ -4,10 +4,14 @@ const outputDiv = document.querySelector("#output")
 function fetchAPI(){
     fetch("https://mystery-api.kushanksriraj.repl.co/get")
     .then((res) => {
-        if(res.status == 401){
+        if(res.status === 401){
             outputDiv.innerText = "You are not looged in"
-        }else{
+        }else if(res.status === 404){
             outputDiv.innerText = "page not found"
+        }else if(res.status === 200){
+            outputDiv.innerText = "you are on the right page"
+        }else{
+            return res.json();
         }
     })
 }
